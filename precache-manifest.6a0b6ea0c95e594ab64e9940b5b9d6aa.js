b71b516761172df715c14f363da90002e2e38113
Joey Steigelman <jhs430@nyu.edu>
2020-12-02 21:43:37 +0100

    Updates

---
+++ precache-manifest.6a0b6ea0c95e594ab64e9940b5b9d6aa.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5c203b68cd8d501446486b19857bcc",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "66eac2b127210de1d82c",
+    "url": "/reactapp/static/css/main.bfe4b74f.chunk.css"
+  },
+  {
+    "revision": "b0b8f263641648687a2d",
+    "url": "/reactapp/static/js/2.80de8ee8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactapp/static/js/2.80de8ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66eac2b127210de1d82c",
+    "url": "/reactapp/static/js/main.61fed39a.chunk.js"
+  },
+  {
+    "revision": "7325850d54f596a589d5",
+    "url": "/reactapp/static/js/runtime-main.f9e8ea19.js"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/reactapp/static/media/Hamburger_icon.86595150.svg"
+  },
+  {
+    "revision": "dff5bed170a650c6f052dc2b12d5b556",
+    "url": "/reactapp/static/media/bills.dff5bed1.png"
+  },
+  {
+    "revision": "e671c05f8e8c0128732ffcfc0b1f61cc",
+    "url": "/reactapp/static/media/box_blue_gallery.e671c05f.jpg"
+  },
+  {
+    "revision": "82f059780ae7aa8c748a103ad2860906",
+    "url": "/reactapp/static/media/box_purple.82f05978.jpg"
+  },
+  {
+    "revision": "34a6a12f52ac3a3c7218ce7d36530699",
+    "url": "/reactapp/static/media/dusk.34a6a12f.jpg"
+  },
+  {
+    "revision": "264db9540e78057d9e46ecc7b992a733",
+    "url": "/reactapp/static/media/hologram_winsor.264db954.jpg"
+  },
+  {
+    "revision": "c2f95485fa1132d52c455fdc7e12012f",
+    "url": "/reactapp/static/media/neon_pinkgreen_banner.c2f95485.jpg"
+  },
+  {
+    "revision": "bb0dd87d5340b83ed5f7a4809f0bee11",
+    "url": "/reactapp/static/media/neon_pinkgreen_light_large.bb0dd87d.jpg"
+  },
+  {
+    "revision": "2951442f9e000ffdb8eb39a99279cd41",
+    "url": "/reactapp/static/media/neon_pinkgreen_light_medium.2951442f.jpg"
+  },
+  {
+    "revision": "1af7134f73fb669a39a143edfc286d93",
+    "url": "/reactapp/static/media/purp.1af7134f.mp4"
+  },
+  {
+    "revision": "f93488a4d583f49bc801dbd1e103abe0",
+    "url": "/reactapp/static/media/purplebox.f93488a4.jpg"
+  },
+  {
+    "revision": "43746a0db69a58e1af2f5bd0d9387381",
+    "url": "/reactapp/static/media/singlescooplowq.43746a0d.jpg"
+  },
+  {
+    "revision": "67d1ca4e68f27a284cdaaf5f4cbe81c3",
+    "url": "/reactapp/static/media/sunset.67d1ca4e.jpg"
+  },
+  {
+    "revision": "fc7a40a738567ab3a25f54b441a48bad",
+    "url": "/reactapp/static/media/sunset_red.fc7a40a7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6d4df120534f2191f82e29a82727351.js"
+  "/reactapp/precache-manifest.6a0b6ea0c95e594ab64e9940b5b9d6aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
