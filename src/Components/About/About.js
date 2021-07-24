e634bb70fe25fd59cab050e630d5c87f09c7c58a
Joey Steigelman <joey.steigelman@gmail.com>
2021-07-24 16:27:22 +0200

    updated about text and work padding

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -19,17 +19,15 @@ export class About extends React.Component {
           </h2>
           <p>
             Guided by an interest in visual perception, Steigelman explores
-            color, light, materiality, and space. These themes are addressed by
-            the artist in a diverse range of media including painting, neon, and
-            acrylic resin. Steigelman has exhibited at galleries, museums, and
-            institutions, and recently participated in group exhibitions at
-            Christie’s, New York (2020), Local Project, New York (2020), The
-            Center for Holographic Arts x Plaxall Gallery, New York (2019),
-            Cluster Gallery, New York (2019), Greenpoint Gallery, New York
-            (2019), Blackbird Gallery, New York (2019), and Gallery 55, Brooklyn
-            (2018). Steigelman was trained in painting at Parsons School of
-            Design and Pratt Institute, and is a graduate of New York
-            University.
+            color, light, materiality, and space. She addresses these themes in
+            a diverse range of media including painting, neon, and acrylic
+            resin. Steigelman has recently exhibited at Christie’s, New York
+            (2020), Local Project, New York (2020), The Center for Holographic
+            Arts x Plaxall Gallery, New York (2019), Cluster Gallery, New York
+            (2019), Greenpoint Gallery, New York (2019), Blackbird Gallery, New
+            York (2019), and Gallery 55, Brooklyn (2018). Steigelman was trained
+            in painting at Parsons School of Design and Pratt Institute, and is
+            a graduate of New York University.
           </p>
           <section className="about-presslinks">
             <p>
@@ -78,9 +76,9 @@ export class About extends React.Component {
             <p>
               Steigelman's work has been featured in Barrons (2019), Creative
               Time (2017), Hamptons Art Hub (2016), and mentioned in STIR
-              (2019). Steigelman's work has also been featured on the official Instagram
-              accounts of Christie's New York, Christie's Los Angeles, and
-              Christie's Paris.{" "}
+              (2019). Steigelman's work has also been featured on the official
+              Instagram accounts of Christie's New York, Christie's Los Angeles,
+              and Christie's Paris.{" "}
             </p>
           </section>
         </div>
