1eb6956fd9dd8ec3fe2fe899248e9a1c5c4e22fa
Joey Steigelman <joey.steigelman@gmail.com>
2021-08-14 12:37:28 +0200

    Updates

---
+++ precache-manifest.61a50ead7c2e9ead41e58ab4a7a0899e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a14d0398b8fafcacf6daeb313ea22ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "115e0d4c394f0c8f4568",
+    "url": "/static/css/main.afb8a01d.chunk.css"
+  },
+  {
+    "revision": "8ce0fa943dd483791f57",
+    "url": "/static/js/2.87a49e03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.87a49e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115e0d4c394f0c8f4568",
+    "url": "/static/js/main.85f73b2e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/static/media/Hamburger_icon.86595150.svg"
+  },
+  {
+    "revision": "312374e1810dc6c5a16b46386c608742",
+    "url": "/static/media/acrylic_podium.312374e1.jpg"
+  },
+  {
+    "revision": "dff5bed170a650c6f052dc2b12d5b556",
+    "url": "/static/media/bills.dff5bed1.png"
+  },
+  {
+    "revision": "b2229e133d9126732aa492c47dad6f3a",
+    "url": "/static/media/box_blue.b2229e13.jpg"
+  },
+  {
+    "revision": "82f059780ae7aa8c748a103ad2860906",
+    "url": "/static/media/box_purple.82f05978.jpg"
+  },
+  {
+    "revision": "34a6a12f52ac3a3c7218ce7d36530699",
+    "url": "/static/media/dusk.34a6a12f.jpg"
+  },
+  {
+    "revision": "264db9540e78057d9e46ecc7b992a733",
+    "url": "/static/media/hologram_winsor.264db954.jpg"
+  },
+  {
+    "revision": "bb0dd87d5340b83ed5f7a4809f0bee11",
+    "url": "/static/media/neon_pinkgreen_light_large.bb0dd87d.jpg"
+  },
+  {
+    "revision": "f93488a4d583f49bc801dbd1e103abe0",
+    "url": "/static/media/purplebox.f93488a4.jpg"
+  },
+  {
+    "revision": "43746a0db69a58e1af2f5bd0d9387381",
+    "url": "/static/media/singlescooplowq.43746a0d.jpg"
+  },
+  {
+    "revision": "36111260e89fc72af117e6a94628008c",
+    "url": "/static/media/sunset_purple.36111260.jpg"
+  },
+  {
+    "revision": "fc7a40a738567ab3a25f54b441a48bad",
+    "url": "/static/media/sunset_red.fc7a40a7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/greencreate.github.io/precache-manifest.fb15b5cead426ff1ee773c6c39d9c25d.js"
+  "/precache-manifest.61a50ead7c2e9ead41e58ab4a7a0899e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/greencreate.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
